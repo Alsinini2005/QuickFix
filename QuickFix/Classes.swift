f0834dc746b20229db1b46859012badd07f88cb8
Mohd202201922 <202201922@student.polytechnic.bh>
2025-12-01 18:00:20 +0300

    Add User Class

--- QuickFix/Classes.swift
+++ QuickFix/Classes.swift
@@ -7,14 +7,16 @@
 
 class User {
     var userID : String
-    var userName : String
+    var name : String
     var email : String
+    var password : String
     var phone : Int
     
-    init(userID: String, userName: String, email: String, phone: Int) {
+    init(userID: String, name: String, email: String,password: String, phone: Int) {
         self.userID = userID
-        self.userName = userName
+        self.name = name
         self.email = email
+        self.password = password
         self.phone = phone
     }
 }
