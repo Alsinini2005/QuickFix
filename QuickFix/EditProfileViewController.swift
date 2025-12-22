3cd2cf2d94e3097ab1bcf300d55fd2522f803568
Faisal <202301457@student.polytechnic.bh>
2025-12-22 14:06:03 +0300

    firebase

---
+++ QuickFix/EditProfileViewController.swift
@@ -0,0 +1,8 @@
+//
+//  EditProfileViewController.swift
+//  QuickFix
+//
+//  Created by BP-36-212-03 on 22/12/2025.
+//
+
+import Foundation
---
+++ QuickFix/SettingsTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsTableViewController.swift
+//  QuickFix
+//
+//  Created by BP-36-212-03 on 22/12/2025.
+//
+
+
+
