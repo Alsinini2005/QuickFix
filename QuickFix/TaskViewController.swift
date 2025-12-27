c414e8e6a1c21ecdf26475858e74befd5a1ac93f
Habib-Hub3 <202200123@student.polytechnic.bh>
2025-12-27 10:18:25 +0300

    Two

--- QuickFix/TaskViewController.swift
+++ QuickFix/TaskViewController.swift
@@ -7,28 +7,13 @@
 
 import UIKit
 
-class TaskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+class TaskViewController: UIViewController, UITableViewDelegate {
+    
     @IBOutlet weak var TableViewA: UITableView!
     @IBOutlet weak var TableViewP: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        TableViewA.delegate = self
-        TableViewA.dataSource = self
-        TableViewP.delegate = self
-        TableViewP.dataSource = self
-       
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-   
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskCellA") as! TaskViewCell
         
-        return cell
     }
-
 }
